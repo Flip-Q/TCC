2965c067bbbb638d18b790d0561f8d766358aad5
Flip-Q <rqfilipe@gmail.com>
2026-05-22 19:17:47 -0300

    Adicao static url

--- Backend/Code/Core/settings.py
+++ Backend/Code/Core/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env_path = BASE_DIR / '.env'
 load_dotenv(dotenv_path=env_path)
 
+
 AMBIENTE_NUVEM = not bool(os.environ.get('POSTGRES_NAME'))
 
 # Quick-start development settings - unsuitable for production
@@ -144,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
